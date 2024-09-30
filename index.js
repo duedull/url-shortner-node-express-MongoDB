d1c503af0ae42704bd023bfe1cda2c9f155074ef
duedull <mr.duedull@gmail.com>
2024-09-30 18:09:57 +0530

    installed ejs

--- index.js
+++ index.js
@@ -3,12 +3,26 @@ const app = express()
 const port = 1000
 const { connect_db } = require('./controllers/url');
 const { router } = require('./routes/url')
+const { new_Schema_url } = require('./models/url')
 
 app.use(express.json());
 app.use('/short', router)
-app.get('/', (req, res) => { res.send('hello world') });
+
+app.get('/', async (req, res) => {
+    const vists = await new_Schema_url.find({});
+    console.log(vists);
+    return res.end(`
+        <html>
+        <body>
+        <ol>
+        ${vists.map(item => `<li>${item.short_id} - ${item.redirect_Url} - ${item.visitHistory.length}</li>`).join('')}
+        </ol>
+        </body>
+        <html>
+        `)
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`)
     connect_db();
-})
+});
